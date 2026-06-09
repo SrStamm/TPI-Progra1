e62c5f63d65d7732137bff45c012de0c19b69e05
Gastón Uriel Fernández <gaston.u.fernandez@hotmail.com>
2026-06-09 20:18:15 -0300

    TPIPROG1-4: corregida funcion agregando persistencia en csv

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os
 import csv
 
 # -----------------------
@@ -53,45 +52,21 @@ def obtener_paises_de_archivo(file_dir = "datos.csv"):
         return lista_paises
 
 # -----------------------
-# Funciones de Gastón
+# Función de Gastón
 # -----------------------
 
-paises = []
+def agregar_pais_a_archivo():
+    # ingreso y validación de nombre
+    nombre = input("Ingrese el nombre del país: ").strip()
+    while not nombre.isalpha():
+        nombre = input("Error Ingrese un nombre válido: ")
 
-def cargar_desde_csv():
-    nombre_archivo = "datos.csv"
-    if os.path.exists(nombre_archivo):   
-        with open (nombre_archivo, "r") as archivo:
-            next(archivo)
-            pass
-            for linea in archivo:
-                datos_pais = linea.split(",")
-                if len(datos_pais) == 4:
-                    try:
-                        nombre = datos_pais[0]
-                        continente = datos_pais[3].strip() # Le sumamos strip para limpiar el salto de línea \n
-                        poblacion = datos_pais[1]
-                        poblacion = int(poblacion)
-                        superficie = datos_pais[2]
-                        superficie = int(superficie)
-                        pais = {
-                            "nombre": nombre,
-                            "continente": continente,
-                            "poblacion": poblacion,
-                            "superficie": superficie,
-                        }
-                        paises.append(pais)
-                    except ValueError as e:
-                        print("Hubo un Error, intenta de nuevo.")   
-    else:
-        print(f"Error: El archivo {nombre_archivo} no fue encontrado.")
-        print("Se iniciará el sistema con la lista de países vacía.")
-    return paises
+    poblacion = input("Ingrese la población: ")
+    superficie = input("Ingrese la superficie: ")
+    continente = input("Ingrese el continente: ")
 
-# Ejecutamos la función para cargar la lista con la que va a trabajar el programa
-paises = cargar_desde_csv()
-
-# Bloque de validación de nombre
-nombre = input("Ingrese el nombre del país: ").strip()
-while not nombre.isalpha():
-    nombre = input("Error Ingrese un nombre válido: ")
+    # escribe lq línea en el CSV usando los datos de arriba
+    with open("datos.csv", "a", encoding="utf-8") as archivo:
+        archivo.write(f"\n{nombre},{poblacion},{superficie},{continente}")
+    
+    print(f"¡Éxito! El país {nombre} fue agregado al archivo.")
